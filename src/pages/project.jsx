cebf2f6181d15fb40c8a93e245823d00712a1caa
aaxzheng <aaronzheng3@gmail.com>
2019-09-18 15:52:22 -0400

    project page and transitions

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React from 'react';
+import '../../public/css/project.css';
+import '../../public/css/main.css';
 
 class Project extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            projects: [1,2,3,4,5,6,7,8],
+            colors: ["red", "green", "gold", "blue", "orange","purple"]
+        };
     }
 
+
+
     render() {
+        let projects;
+        projects = this.state.projects.map((project,idx) => {
+            let color = this.state.colors[idx % 6];
+            return (
+                <div key={idx} className={"project-box-main " + color}>
+
+                </div>
+            )
+        })
         return (
-            <div >
-                Project Page
+            <div className="project-main">
+                <div className="project-wrap wrapper">
+                    <div className="project-intro">
+                        My Projects
+                    </div>
+                    <div className="project-list">
+                        {projects}
+                    </div>
+                </div>
             </div>
         );
     }
